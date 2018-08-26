3ecc03539d788dcdc859d8661e8ed88bfc06698d
MichaelBurak <michaelrburak@gmail.com>
2018-08-26 12:35:24 -0400

    CReactA boilerplate, bulma compiled SASS, footer demo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Foot from "./components/Foot";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Foot />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Foot.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Foot extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Logos</strong>
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Foot;
